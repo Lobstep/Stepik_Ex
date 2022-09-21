c54181b20e5fe0d656168f6aecba91279bc7f270
Lobstep <79393999+Lobstep@users.noreply.github.com>
2022-09-21 18:25:42 +0300

    Delete ex3.4.5 .py

--- ex3.4.5 .py
+++
@@ -1,3 +0,0 @@
-# Используя raw-строки, задайте строку, содержащую этот путь к файлу: C:\WINDOWS\System32\drivers\etc\hosts.
-# Результат отобразите на экране.
-print(r'C:\WINDOWS\System32\drivers\etc\hosts')
